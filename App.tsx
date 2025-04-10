6af2385570439a1540b97f6b6da5aac023b78dbd
stepan.rutz <stepan.rutz@gmx.de>
2025-04-10 13:06:51 +0200

    counter

--- App.tsx
+++ App.tsx
@@ -1,17 +1,37 @@
 import "./global.css"
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ReactNode, useState } from "react";
+import { Button, StyleProp, StyleSheet, Text, TextStyle, View, ViewStyle } from 'react-native';
+
+function BigText({ children } : { children: ReactNode }) {
+    return (
+        <Text className="text-4xl bg-neutral-300">
+            {children}
+        </Text>
+    )
+}
 
 
 export default function App() {
+    const [ count, setCount ] = useState(() => {
+        return 1
+    })
+    console.log("render app...", count)
     return (
-        <View style={styles.container}>
-            <View className="h-4 bg-red-300">
-                <Text className="bg-red-300">Hello World!</Text>
-            </View>
-            <StatusBar style="auto" />
+    <View className="flex-1 flex justify-center items-center">
+        <View className="flex gap-2 " >
+            <BigText>Count {count}</BigText>
+            <Button title="Increment" 
+                onPress={() => {
+                    setCount((oldCount) => { return oldCount + 1 })
+                    console.log("button clicked")
+                    setCount((oldCount) => oldCount + 1)
+                    setCount((oldCount) => oldCount + 1)
+                }} />
         </View>
+        <StatusBar style="auto" />
+    </View>
     )
 }
 
