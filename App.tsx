f21b8305bcdb6bd8c7ac45492194c0a19cad90f3
stepan.rutz <stepan.rutz@gmx.de>
2025-04-10 08:13:15 +0200

    dimensions listener

--- App.tsx
+++ App.tsx
@@ -1,14 +1,37 @@
 import "./global.css"
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Dimensions, ScaledSize, StyleSheet, Text, View } from 'react-native';
 
 
 export default function App() {
+    const [ dimensions, setDimensions] = useState(() => {
+        const { width, height } = Dimensions.get("window")
+        return {
+            width,
+            height,
+            portrait: height > width,
+        }
+    })
+    useEffect(() => {
+        const l = ({ window } : { window: ScaledSize, screen: ScaledSize}) => {
+            console.log("Window size changed", window.width, window.height)
+            setDimensions({
+                width: window.width,
+                height: window.height,
+                portrait: window.height > window.width,
+            })
+        }
+        const sub = Dimensions.addEventListener("change", l)
+        return () => sub.remove() 
+    }, [])
+    console.log("rerender", dimensions)
     return (
         <View style={styles.container}>
             <View className="h-4 bg-red-300">
                 <Text className="bg-red-300">Hello World!</Text>
+                <Text className="bg-neutral-300 text-2xl whitespace-pre">{JSON.stringify(dimensions, null, 2)}</Text>
             </View>
             <StatusBar style="auto" />
         </View>
