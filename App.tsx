692fa964941aad11db171a5d9f199a7e1a9280f8
stepan.rutz <stepan.rutz@gmx.de>
2025-04-10 14:35:42 +0200

    more

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import "./global.css"
 
 import { StatusBar } from 'expo-status-bar';
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Button, StyleProp, StyleSheet, Text, TextStyle, View, ViewStyle } from 'react-native';
+
 
 function BigText({ children } : { children: ReactNode }) {
     return (
@@ -12,23 +13,32 @@ function BigText({ children } : { children: ReactNode }) {
     )
 }
 
+function useInterval(f: () => void, delay: number) {
+    useEffect(() => {
+        const id = setInterval(f, delay)
+        return () => {
+            clearInterval(id)
+        }
+    }, [])
+}
+
+function useTime(delay: number) {
+    const [time, setTime ] = useState(new Date())
+    useInterval(() => setTime(new Date()), delay)
+    const a = { age: 17, iq: 27}
+    const b = structuredClone(a)
+    return time
+}
 
 export default function App() {
-    const [ count, setCount ] = useState(() => {
-        return 1
-    })
-    console.log("render app...", count)
+    const time = useTime(3_600_000)
     return (
     <View className="flex-1 flex justify-center items-center">
-        <View className="flex gap-2 " >
-            <BigText>Count {count}</BigText>
-            <Button title="Increment" 
-                onPress={() => {
-                    setCount((oldCount) => { return oldCount + 1 })
-                    console.log("button clicked")
-                    setCount((oldCount) => oldCount + 1)
-                    setCount((oldCount) => oldCount + 1)
-                }} />
+        <View className="flex gap-2 "   >
+            <BigText>{time.toTimeString()}</BigText>
+            <Button title="butt" onPress={() => {
+                const [stuff, setStuff] = useState(0)
+            }}/>
         </View>
         <StatusBar style="auto" />
     </View>
